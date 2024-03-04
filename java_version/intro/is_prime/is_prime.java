9e3e3d9e0132b24545e613a6d08cf436d007d1fe
miker704 <miker704@gmail.com>
2024-03-04 07:37:09 -0500

    problem setup

---
+++ java_version/intro/is_prime/is_prime.java
@@ -0,0 +1,25 @@
+package intro.is_prime;
+
+public class is_prime {
+
+    public static boolean isPrime(int number) {
+    
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPrime(2)); // true
+        System.out.println(isPrime(3)); // true
+        System.out.println(isPrime(4)); // false
+        System.out.println(isPrime(5)); // true
+        System.out.println(isPrime(6)); // false
+        System.out.println(isPrime(7)); // true
+        System.out.println(isPrime(8)); // false
+        System.out.println(isPrime(25)); // false
+        System.out.println(isPrime(31)); // true
+        System.out.println(isPrime(2017)); // true
+        System.out.println(isPrime(2048)); // false
+        System.out.println(isPrime(1)); // false
+        System.out.println(isPrime(713)); // false
+    }
+}
