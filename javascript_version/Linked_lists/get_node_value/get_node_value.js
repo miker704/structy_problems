fdac9fc0b3139b985866cad22b1bca699a856ac1
miker704 <miker704@gmail.com>
2024-03-04 03:28:33 -0500

    problem setup

---
+++ javascript_version/Linked_lists/get_node_value/get_node_value.js
@@ -0,0 +1,75 @@
+// get node value
+// Write a function, getNodeValue, that takes in a pointer to the head of a linked list and an index. 
+// The function should return the value of the linked list at the specified index.
+
+// If there is no node at the given index, then return the empty string.
+class Node {
+    constructor (value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+
+const getNodeValue = (head, index) => {
+
+}
+
+const getNodeValueRecur = (head, index) => {
+
+}
+
+const _getNodeValueRecur = (curr, index) => {
+
+}
+
+
+
+
+
+console.log("iterative method");
+
+const a = new Node("a");
+const b = new Node("b");
+const c = new Node("c");
+const d = new Node("d");
+
+a.next = b;
+b.next = c;
+c.next = d;
+
+// a -> b -> c -> d
+
+console.log(getNodeValue(a, 2)); // "c"
+console.log(getNodeValue(a, 3)); // "d"
+
+// a -> b -> c -> d
+
+console.log(getNodeValue(a, 7)); // "" null
+console.log("recursion method");
+console.log(getNodeValueRecur(a, 2)); // "c"
+console.log(getNodeValueRecur(a, 3)); // "d"
+
+// a -> b -> c -> d
+
+console.log(getNodeValueRecur(a, 7)); // "" null
+
+const node1 = new Node("banana");
+const node2 = new Node("mango");
+
+node1.next = node2;
+
+
+// banana -> mango
+console.log("iterative method");
+
+console.log(getNodeValue(node1, 0)); // "banana"
+// banana -> mango
+
+console.log(getNodeValue(node1, 1)); // "mango"
+
+console.log("recursion method");
+console.log(getNodeValueRecur(node1, 0)); // "banana"
+// banana -> mango
+
+console.log(getNodeValueRecur(node1, 1)); // "mango"
