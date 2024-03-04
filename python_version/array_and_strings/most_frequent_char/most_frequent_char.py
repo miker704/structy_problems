ed119d1d182ce9a2f4ba15c25d4e02e849f88be0
miker704 <miker704@gmail.com>
2024-03-04 05:11:05 -0500

    problem setup

---
+++ python_version/array_and_strings/most_frequent_char/most_frequent_char.py
@@ -0,0 +1,19 @@
+# most frequent char
+# Write a function, mostFrequentChar, that takes in a string as an argument.
+# The function should return the most frequent character of the string.
+# If there are ties, return the character that appears earlier in the string.
+
+# You can assume that the input string is non-empty.
+
+def most_frequent_char(s):
+    return
+
+
+
+print(most_frequent_char('bookeeper'))  # -> 'e'
+print(most_frequent_char('david'))  # -> 'd'
+print(most_frequent_char('abby'))  # -> 'b'
+print(most_frequent_char('mississippi'))  # -> 'i'
+print(most_frequent_char('potato'))  # -> 'o'
+print(most_frequent_char('eleventennine'))  # -> 'e'
+print(most_frequent_char('riverbed'))  # -> 'r'
