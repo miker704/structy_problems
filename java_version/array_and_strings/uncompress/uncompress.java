fa0c1f01acc8b720856977cad32fd18eeef0144b
miker704 <miker704@gmail.com>
2024-03-04 07:35:56 -0500

    problem setup

---
+++ java_version/array_and_strings/uncompress/uncompress.java
@@ -0,0 +1,57 @@
+package array_and_strings.uncompress;
+
+// uncompress
+// Write a function, uncompress, that takes in a string as an argument. The input string 
+// will be formatted into multiple groups according to the following pattern:
+
+// <number><char>
+
+// for example, '2c' or '3a'.
+// The function should return an uncompressed version of the string where each 'char' of a group is repeated 'number' times consecutively. 
+// You may assume that the input string is well-formed according to the previously mentioned pattern.
+
+public class uncompress {
+
+    // public static String Uncompress(String str) {
+
+    // String result = "";
+    // int i = 0;
+    // int j = 0;
+    // String numbers = "0123456789";
+    // String num = "";
+    // while (i < str.length()) {
+
+    // if (numbers.indexOf(str.charAt(i)) != -1) {
+    // num += str.charAt(i);
+    // } else if (numbers.indexOf(str.charAt(i)) == -1) {
+    // int counter = Integer.parseInt(num);
+    // for (int x = 0; x < counter; x++) {
+    // result += str.charAt(i);
+    // }
+    // num = "";
+    // }
+    // i++;
+    // j++;
+    // }
+
+    // return result;
+    // }
+
+    // optimized
+
+    public static String Uncompress(String str) {
+
+        String result = "";
+    
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Uncompress("2c3a1t")); // -> 'ccaaat'
+        System.out.println(Uncompress("4s2b")); // -> 'ssssbb'
+        System.out.println(Uncompress("2p1o5p")); // -> 'ppoppppp'
+        System.out.println(Uncompress("3n12e2z")); // -> 'nnneeeeeeeeeeeezz'
+        System.out.println(Uncompress("127y")); // ->'yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy'
+    }
+
+}
