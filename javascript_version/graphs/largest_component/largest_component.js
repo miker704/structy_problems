4773af4f0c7de01f4abb4ed2b95ee0a37cdb4fcd
miker704 <miker704@gmail.com>
2024-03-04 03:28:01 -0500

    problem setup

---
+++ javascript_version/graphs/largest_component/largest_component.js
@@ -0,0 +1,56 @@
+// largest component
+// Write a function, largestComponent, that takes in an unordered_map representing the adjacency
+//  list of an undirected graph.
+//  The function should return the size of the largest connected component in the graph.
+
+const largestComponent = (graph) => {
+    // todo
+    
+  };
+  
+  const hasPath = (graph, currNode, visitedNodes) => {
+  
+    
+  
+  }
+  
+  
+  console.log(largestComponent({
+    0: ['8', '1', '5'],
+    1: ['0'],
+    5: ['0', '8'],
+    8: ['0', '5'],
+    2: ['3', '4'],
+    3: ['2', '4'],
+    4: ['3', '2']
+  })); // -> 4
+  console.log(largestComponent({
+    1: ['2'],
+    2: ['1', '8'],
+    6: ['7'],
+    9: ['8'],
+    7: ['6', '8'],
+    8: ['9', '7', '2']
+  })); // -> 6
+  console.log(largestComponent({
+    3: [],
+    4: ['6'],
+    6: ['4', '5', '7', '8'],
+    8: ['6'],
+    7: ['6'],
+    5: ['6'],
+    1: ['2'],
+    2: ['1']
+  }));// -> 5
+  console.log(largestComponent({}));// -> 0
+  console.log(largestComponent({
+    0: ['4', '7'],
+    1: [],
+    2: [],
+    3: ['6'],
+    4: ['0'],
+    6: ['3'],
+    7: ['0'],
+    8: []
+  }));// -> 3
+  
