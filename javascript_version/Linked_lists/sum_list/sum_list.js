215ce959ebf02d6f8c0e63a74b11b5683e62b9db
miker704 <miker704@gmail.com>
2024-03-04 03:28:57 -0500

    problem setup

---
+++ javascript_version/Linked_lists/sum_list/sum_list.js
@@ -0,0 +1,67 @@
+// sum list
+// Write a function, sumList, that takes in the head of a linked list containing numbers as an argument. 
+// The function should return the total sum of all values in the linked list.
+
+
+class Node {
+    constructor(value){
+        this.value = value;
+        this.next = null;
+    }
+}
+
+
+const sumList = (head) => {
+
+}
+
+const sumListRecur = (head) => {
+
+}
+
+
+const _sumListRecur = (curr) => {
+
+}
+
+console.log("iterative method");
+
+const a = new Node(2);
+const b = new Node(8);
+const c = new Node(3);
+const d = new Node(-1);
+const e = new Node(7);
+
+a.next = b;
+b.next = c;
+c.next = d;
+d.next = e;
+
+// 2 -> 8 -> 3 -> -1 -> 7
+
+console.log(sumList(a)); // 19
+
+
+const x = new Node(38);
+const y = new Node(4);
+
+x.next = y;
+
+// 38 -> 4
+
+console.log(sumList(x)); // 42
+
+const z = new Node(100);
+
+// 100
+
+console.log(sumList(z)); // 100
+console.log(sumList(null)); // 0
+
+
+
+console.log("recursion method");
+console.log(sumListRecur(a)); // 19
+console.log(sumListRecur(x)); // 42
+console.log(sumListRecur(z)); // 100
+console.log(sumListRecur(null)); // 0
