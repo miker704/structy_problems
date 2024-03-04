59f828787f3236e3b3a2f25fd50d0ec9e8e54fb3
miker704 <miker704@gmail.com>
2024-03-04 05:28:29 -0500

    problem setup

---
+++ python_version/dynamic_programming/overlap_subsequence/overlap_subsequence.py
@@ -0,0 +1,25 @@
+# overlap subsequence
+# Write a function, overlapSubsequence, that takes in two strings as arguments.
+#  The function should return the length of the longest overlapping subsequence.
+
+# A subsequence of a string can be created by deleting any characters of the string,
+#  while maintaining the relative order of characters.
+
+def overlap_subsequence(string_1, string_2):
+    return 
+
+
+def _overlap_subsequence(string_1, string_2, i, j, indexes):
+
+    return 
+
+
+print(overlap_subsequence("dogs", "daogt"))  # -> 3
+print(overlap_subsequence("xcyats", "criaotsi"))  # -> 4
+print(overlap_subsequence("xfeqortsver", "feeeuavoeqr"))  # -> 5
+print(overlap_subsequence("kinfolklivemustache",
+      "bespokekinfolksnackwave"))  # -> 11
+print(overlap_subsequence(
+    "mumblecorebeardleggingsauthenticunicorn",
+    "succulentspughumblemeditationlocavore"
+))  # -> 15
