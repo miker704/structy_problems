00966fe4d8201ab79a7cac4a3804e6f58a170104
miker704 <miker704@gmail.com>
2024-03-04 05:47:05 -0500

    problem setup

---
+++ python_version/mixed_recall/combine_intervals/combine_intervals.py
@@ -0,0 +1,80 @@
+# combine intervals
+# Write a function, combineIntervals, that takes in a vector of intervals as an argument.
+#  Each interval is a vector containing a pair of numbers representing a start and end time.
+#  Your function should combine overlapping intervals and return a vector containing the
+#  combined intervals.
+
+# For example:
+
+# Given two intervals:
+
+# {1, 4} and {3, 7}
+
+# The intervals overlap and
+# should be combined into:
+
+# {1, 7}
+# You may return the combined intervals in any order.
+
+# You can assume that the input vector contains at least one interval and all
+#  intervals are valid with start < end.
+
+def combine_intervals(intervals):
+
+    return 
+
+
+intervals = [
+    (1, 4),
+    (12, 15),
+    (3, 7),
+    (8, 13),
+]
+print(combine_intervals(intervals))
+# -> [ (1, 7), (8, 15) ]
+intervals = [
+    (6, 8),
+    (2, 9),
+    (10, 12),
+    (20, 24),
+]
+print(combine_intervals(intervals))
+# -> [ (2, 9), (10, 12), (20, 24) ]
+intervals = [
+    (3, 7),
+    (5, 8),
+    (1, 5),
+]
+print(combine_intervals(intervals))
+# -> [ (1, 8) ]
+intervals = [
+    (3, 7),
+    (10, 13),
+    (5, 8),
+    (27, 31),
+    (1, 5),
+    (12, 16),
+    (20, 22),
+]
+print(combine_intervals(intervals))
+# -> [ (1, 8), (10, 16), (20, 22), (27, 31) ]
+intervals = [
+    (3, 7),
+    (10, 13),
+    (5, 8),
+    (27, 31),
+    (1, 5),
+    (12, 16),
+    (20, 32),
+]
+print(combine_intervals(intervals))
+# -> [ (1, 8), (10, 16), (20, 32) ]
+intervals = [
+    (64, 70),
+    (50, 55),
+    (62, 65),
+    (12, 50),
+    (72, 300000),
+]
+print(combine_intervals(intervals))
+# -> [ (12, 55), (62, 70), (72, 300000) ]
