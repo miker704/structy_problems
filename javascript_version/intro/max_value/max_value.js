a7f97cf77ed582a1bd39a89ff8330938cca2c295
miker704 <miker704@gmail.com>
2024-03-04 03:28:25 -0500

    problem setup

---
+++ javascript_version/intro/max_value/max_value.js
@@ -0,0 +1,22 @@
+// Max Value
+// Write a function, maxValue, that takes in array of numbers as an argument. 
+// The function should return the largest number in the array.
+
+// Solve this without using any built-in array methods.
+
+// You can assume that the array is non-empty.
+
+
+const maxValue = (array) => {
+}
+
+
+
+
+
+console.log(maxValue([10, 5, 40, 40.3])); // 40.3
+console.log(maxValue([-5, -2, -1, -11])); // -1
+console.log(maxValue([42])); // 42
+console.log(maxValue([1000,8])); // 1000
+console.log(maxValue([1000, 8, 9000])); // 9000
+console.log(maxValue([2, 5, 1, 1, 4])); // 5
