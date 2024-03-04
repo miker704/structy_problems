f90955fd238e034d4b4f60fd7cf324d329dd5a41
miker704 <miker704@gmail.com>
2024-03-04 05:13:36 -0500

    problem setup

---
+++ python_version/intro/hey_programmer/hey_programmer.py
@@ -0,0 +1,9 @@
+# write a functions that takes in a param string s and returns a string 'hey s'
+
+def greet(s):
+    return
+
+if __name__== '__main__':
+    print(greet("alvin")) #'hey alvin'
+    print(greet("jason")) #'hey jason'
+    print(greet("how now brown cow")) #'hey how now brown cow'
