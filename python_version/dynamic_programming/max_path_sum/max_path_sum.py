225532ab50a55c1cc4b71d6cdeba74e1b8f6d611
miker704 <miker704@gmail.com>
2024-03-04 05:28:08 -0500

    problem setup

---
+++ python_version/dynamic_programming/max_path_sum/max_path_sum.py
@@ -0,0 +1,92 @@
+# max path sum
+# Write a function, max_path_sum, that takes in a grid as an argument. 
+# The function should return the maximum sum possible by traveling a path from 
+# the top-left corner to the bottom-right corner. You may only travel through the 
+# grid by moving down or right.
+
+# You can assume that all numbers are non-negative.
+#unoptimized method
+# unoptimized method running at O(2^(r+c)) where 2 is the number of branches right and down directions
+# r is the number of rows and c is the number of columns in the 2d array space complixity is O(r*c)
+
+# def max_path_sum(grid,row=0,col=0):
+#     if row == len(grid) or col == len(grid[0]):
+#         return 0
+#     maxSum = 0
+#     if row <= len(grid)-1 and col <= len(grid[0])-1:
+#         right= max_path_sum(grid,row,col+1)
+#         down = max_path_sum(grid,row+1,col)
+#         maxSum += down+ grid[row][col] if down > right else right+grid[row][col]
+        
+#     return maxSum
+
+# using memoization more optimized method
+# running at O((r*c)) where 2 is the number of branches right and down directions
+# r is the number of rows and c is the number of columns in the 2d array space complixity is O(r+c)
+
+def max_path_sum(grid):
+    return 
+
+def _max_path_sum(grid,row=0,col=0,hsh={}):
+
+    return 
+
+
+grid = [
+  [1, 3, 12],
+  [5, 1, 1],
+  [3, 6, 1],
+]
+print(max_path_sum(grid)) # -> 18
+
+grid = [
+  [1, 2, 8,  1],
+  [3, 1, 12, 10],
+  [4, 0, 6,  3],
+]
+print(max_path_sum(grid))  # -> 36
+
+grid = [
+  [1, 2, 8, 1],
+  [3, 10, 12, 10],
+  [4, 0, 6, 3],
+]
+print(max_path_sum(grid))  # -> 39
+
+grid = [
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+]
+print(max_path_sum(grid))  # -> 27
+
+grid = [
+  [1, 1, 3, 1, 1, 1, 1, 4, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 2, 1, 1, 6, 1, 1, 5, 1, 1, 0, 0, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 5, 1, 1, 1, 1, 0, 1, 1, 1, 1],
+  [2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [2, 1, 1, 1, 1, 8, 1, 1, 1, 1, 1, 1, 1],
+  [2, 1, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1, 9, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 8, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+]
+print(max_path_sum(grid))  # -> 56
