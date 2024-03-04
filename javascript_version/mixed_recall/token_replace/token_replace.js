8d44929f592e197a7ebb3c26096b611aa8504bcd
miker704 <miker704@gmail.com>
2024-03-04 03:30:04 -0500

    problem setup

---
+++ javascript_version/mixed_recall/token_replace/token_replace.js
@@ -0,0 +1,54 @@
+// token replace
+// Write a function, tokenReplace, that takes in an unordered_map of tokens
+//  and a string. The function should return a new string where tokens are replaced.
+
+// Tokens are enclosed in a pair of "$". You can assume that the input string 
+// is properly formatted. Tokens should be replaced from left to right in the string (see test_05)
+
+
+const tokenReplace = (s, tokens) => {
+    // todo
+
+};
+
+
+
+let tokens = {
+    '$LOCATION$': 'park',
+    '$ANIMAL$': 'dog',
+};
+console.log(tokenReplace('Walk the $ANIMAL$ in the $LOCATION$!', tokens));
+// -> 'Walk the dog in the park!'
+tokens = {
+    '$ADJECTIVE$': 'quick',
+    '$VERB$': 'hopped',
+    '$DIRECTION$': 'North'
+};
+console.log(tokenReplace('the $ADJECTIVE$ fox $VERB$ $ADJECTIVE$ly $DIRECTION$ward', tokens));
+// -> 'the quick fox hopped quickly Northward'
+tokens = {
+    '$greeting$': 'hey programmer',
+};
+console.log(tokenReplace('his greeting is always $greeting$.', tokens));
+// -> 'his greeting is always hey programmer.'
+tokens = {
+    '$A$': 'lions',
+    '$B$': 'tigers',
+    '$C$': 'bears',
+};
+console.log(tokenReplace('$A$$B$$C$, oh my.', tokens));
+// -> 'lionstigersbears, oh my.'
+tokens = {
+    '$A$': 'lions',
+    '$B$': 'tigers',
+    '$C$': 'bears',
+};
+console.log(tokenReplace('$B$', tokens));
+// -> 'tigers'
+tokens = {
+    '$second$': 'beta',
+    '$first$': 'alpha',
+    '$third$': 'gamma',
+};
+console.log(tokenReplace('$first$second$third$', tokens));
+// -> 'alphasecondgamma'
