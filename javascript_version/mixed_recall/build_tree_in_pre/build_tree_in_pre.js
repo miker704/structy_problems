8bc9598b1be1cf684dbd3deacbab8050cdadd58b
miker704 <miker704@gmail.com>
2024-03-04 03:29:14 -0500

    problem setup

---
+++ javascript_version/mixed_recall/build_tree_in_pre/build_tree_in_pre.js
@@ -0,0 +1,84 @@
+// build tree in pre
+// Write a function, buildTreeInPre, that takes in a vector of in-ordered values
+//  and a vector of pre-ordered values for a binary tree. The function should build 
+//  a binary tree that has the given in-order and pre-order traversal structure.
+//  The function should return the root of this tree.
+
+// You can assume that the values of inorder and preorder are unique.
+
+// Be sure to allocate the nodes of your tree dynamically with the new keyword.
+
+
+class Node {
+    constructor (val) {
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+const buildTreeInPre = (inOrder, preOrder) => {
+    // todo
+
+
+
+};
+const printInorder = (root) => {
+    if (root === null) {
+        return [];
+    }
+    let left = printInorder(root.left);
+    let right = printInorder(root.right);
+    return [...left, root.val, ...right];
+
+}
+
+console.log(printInorder(buildTreeInPre(
+    ['z', 'y', 'x'],
+    ['y', 'z', 'x']
+)));
+//       y
+//    /    \
+//   z      x
+console.log(printInorder(buildTreeInPre(
+    ['y', 'z', 'x'],
+    ['y', 'x', 'z']
+)));
+//       y
+//        \
+//         x
+//        / 
+//       z
+console.log(printInorder(buildTreeInPre(
+    ['d', 'b', 'g', 'e', 'h', 'a', 'c', 'f'],
+    ['a', 'b', 'd', 'e', 'g', 'h', 'c', 'f']
+)));
+//       a
+//    /    \
+//   b      c
+//  / \      \
+// d   e      f
+//    / \
+//    g  h
+console.log(printInorder(buildTreeInPre(
+    ['t', 'u', 's', 'q', 'r', 'p'],
+    ['u', 't', 's', 'r', 'q', 'p']
+)));
+//     u
+//  /    \
+// t      s
+//         \
+//         r
+//        / \
+//        q  p
+console.log(printInorder(buildTreeInPre(
+    ['m', 'l', 'q', 'o', 'r', 'n', 's', 'p', 't'],
+    ['l', 'm', 'n', 'o', 'q', 'r', 'p', 's', 't']
+)));
+  //        l
+  //     /     \
+  //    m       n
+  //         /    \
+  //         o     p
+  //        / \   / \
+  //       q   r s   t
