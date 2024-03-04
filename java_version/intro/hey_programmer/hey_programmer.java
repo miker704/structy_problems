fb8c3a0db5d17c92866974d33ff6a6602a0553d8
miker704 <miker704@gmail.com>
2024-03-04 07:37:06 -0500

    problem setup

---
+++ java_version/intro/hey_programmer/hey_programmer.java
@@ -0,0 +1,19 @@
+package intro.hey_programmer;
+
+//write a functions that takes in a param string s and returns a string 'hey s';
+
+public class hey_programmer {
+
+    static String greet(String s) {
+        return "";
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(hey_programmer.greet("alvin")); // -> 'hey alvin'
+        System.out.println(hey_programmer.greet("jason")); // -> 'hey jason'
+        System.out.println(hey_programmer.greet("how now brown cow")); // -> 'hey how now brown cow'
+
+    }
+
+}
