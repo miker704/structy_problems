23f834c04c2d165db761a3c06b48d9615c1dfc60
miker704 <miker704@gmail.com>
2024-03-04 03:27:30 -0500

    problem setup

---
+++ javascript_version/dynamic_programming/tribonacci/tribonacci.js
@@ -0,0 +1,26 @@
+// tribonacci
+// Write a function tribonacci that takes in a number argument, n, 
+//and returns the n-th number of the Tribonacci sequence.
+
+// The 0-th and 1-st numbers of the sequence are both 0.
+
+// The 2-nd number of the sequence is 1.
+
+// To generate further numbers of the sequence, calculate the sum of previous three numbers.
+
+// Solve this recursively.
+
+const tribonacci = (n, hash = {}) => {
+    
+
+}
+
+
+console.log(tribonacci(0)); // 0
+console.log(tribonacci(1)); // 0
+console.log(tribonacci(2)); // 1
+console.log(tribonacci(5)); // 4
+console.log(tribonacci(7)); // 13
+console.log(tribonacci(14)); // 927
+console.log(tribonacci(20)); // 35890
+console.log(tribonacci(37)); // 1132436852
