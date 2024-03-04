661204992e4ed83aae6b2d8ea6aaec85b3a42d67
miker704 <miker704@gmail.com>
2024-03-04 05:13:40 -0500

    problem setup

---
+++ python_version/intro/is_prime/is_prime.py
@@ -0,0 +1,31 @@
+# is prime
+# Write a function, is_prime, that takes in a number as an argument. 
+# The function should return a boolean indicating whether or not 
+# the given number is prime.
+
+# A prime number is a number that is only divisible by two distinct numbers: 1 and itself.
+
+# For example, 7 is a prime because it is only divisible by 1 and 7. For example, 
+# 6 is not a prime because it is divisible by 1, 2, 3, and 6.
+
+# You can assume that the input number is a positive integer.
+
+
+def is_prime(num):
+
+    return 
+
+
+print(is_prime(2)) #true
+print(is_prime(3)) #true
+print(is_prime(4)) #false
+print(is_prime(5)) # true
+print(is_prime(6)) # false
+print(is_prime(7)) # true
+print(is_prime(8)) # false
+print(is_prime(25)) #false
+print(is_prime(31)) # true
+print(is_prime(2017)) #true 
+print(is_prime(2048)) #false
+print(is_prime(1)) # false
+print(is_prime(713)) #false
