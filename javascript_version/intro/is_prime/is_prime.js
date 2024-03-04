2a94daf3eba34061f9b235c575a5d974374a5470
miker704 <miker704@gmail.com>
2024-03-04 03:28:23 -0500

    problem setup

---
+++ javascript_version/intro/is_prime/is_prime.js
@@ -0,0 +1,29 @@
+// is prime
+// Write a function, isPrime, that takes in a number as an argument. The function should
+//  return a boolean indicating whether or not the given number is prime.
+
+// A prime number is a number that is only divisible by two distinct 
+// numbers: 1 and itself.
+
+// For example, 7 is a prime because it is only divisible by 1 and 7. For example, 
+// 6 is not a prime because it is divisible by 1, 2, 3, and 6.
+
+// You can assume that the input number is a positive integer.
+
+const isPrime = (n) => {
+
+}
+
+console.log(isPrime(2)); //true
+console.log(isPrime(3)); //trues
+console.log(isPrime(4)); //false
+console.log(isPrime(5)); // true
+console.log(isPrime(6)); // false
+console.log(isPrime(7)); // true
+console.log(isPrime(8)); // false
+console.log(isPrime(25)); //false
+console.log(isPrime(31)); // true
+console.log(isPrime(2017)); //true 
+console.log(isPrime(2048)); //false
+console.log(isPrime(1)); // false
+console.log(isPrime(713)); //false
