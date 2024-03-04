52a57c4b8b02a719a54166e60d34157587701db1
miker704 <miker704@gmail.com>
2024-03-04 05:10:42 -0500

    problem setup

---
+++ python_version/array_and_strings/anagrams/anagrams.py
@@ -0,0 +1,25 @@
+# anagrams
+# Write a function, anagrams, that takes in two strings as arguments. The function should return
+#  a boolean indicating whether or not the strings are anagrams. Anagrams are strings that
+# contain the same characters, but in any order.
+
+def anagrams(s1, s2):
+   return
+ 
+
+
+def hsh_it(s):
+    return 
+
+
+print(anagrams('restful', 'fluster'))  # -> True
+print(anagrams('cats', 'tocs'))  # -> False
+print(anagrams('monkeyswrite', 'newyorktimes'))  # -> True
+print(anagrams('paper', 'reapa'))  # -> False
+print(anagrams('elbow', 'below'))  # -> True
+print(anagrams('tax', 'taxi'))  # -> False
+print(anagrams('taxi', 'tax'))  # -> False
+print(anagrams('night', 'thing'))  # -> True
+print(anagrams('abbc', 'aabc'))  # -> False
+print(anagrams('po', 'popp'))  # -> false
+print(anagrams('pp', 'oo'))  # -> false
