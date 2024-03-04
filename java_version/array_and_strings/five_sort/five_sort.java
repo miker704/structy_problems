f338a22576f9866beb1d9065dd6b0b17728690aa
miker704 <miker704@gmail.com>
2024-03-04 07:35:22 -0500

    problem setup

---
+++ java_version/array_and_strings/five_sort/five_sort.java
@@ -0,0 +1,55 @@
+package array_and_strings.five_sort;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+// five sort
+// Write a function, five_sort, that takes in a list of numbers as an argument.
+// The function should rearrange elements of the list such that all 5s appear at the end.
+// Your function should perform this operation in-place by mutating the original list.
+//  The function should return the list.
+public class five_sort {
+
+    public static List<Integer> fiveSort(List<Integer> vector) {
+    
+
+        return vector;
+    }
+
+    public static void printVector(List<Integer> vector) {
+        System.out.print("[ ");
+        vector.stream().forEach(i -> {
+            System.out.printf("%s,", i);
+        });
+        System.out.println(" ]");
+    }
+
+    public static void main(String[] args) {
+        List<Integer> numbers = new ArrayList<>(Arrays.asList(12, 5, 1, 5, 12, 7));
+        printVector(fiveSort(numbers));
+        // -> [12, 7, 1, 12, 5, 5]
+        numbers = new ArrayList<>(Arrays.asList(5, 2, 5, 6, 5, 1, 10, 2, 5, 5));
+        printVector(fiveSort(numbers));
+        // -> [2, 2, 10, 6, 1, 5, 5, 5, 5, 5]
+        numbers = new ArrayList<>(Arrays.asList(5, 5, 5, 1, 1, 1, 4));
+        printVector(fiveSort(numbers));
+        // -> [4, 1, 1, 1, 5, 5, 5]
+        numbers = new ArrayList<>(Arrays.asList(5, 5, 6, 5, 5, 5, 5));
+        printVector(fiveSort(numbers));
+        // -> [6, 5, 5, 5, 5, 5, 5]
+        numbers = new ArrayList<>(Arrays.asList(5, 1, 2, 5, 5, 3, 2, 5, 1, 5, 5, 5, 4, 5));
+        printVector(fiveSort(numbers));
+        // -> [4, 1, 2, 1, 2, 3, 5, 5, 5, 5, 5, 5, 5, 5]
+
+        List<Integer> fives = new ArrayList<>(Collections.nCopies(20000, 5));
+        List<Integer> fours = new ArrayList<>(Collections.nCopies(20000, 4));
+        List<Integer> nums = new ArrayList<>(fives.size() + fours.size());
+        nums.addAll(fives);
+        nums.addAll(fours);
+//        printVector(fiveSort(nums));
+        // twenty-thousand 4s followed by twenty-thousand 5s
+        // -> [4, 4, 4, 4, ..., 5, 5, 5, 5]
+    }
+}
