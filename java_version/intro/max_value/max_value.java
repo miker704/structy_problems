684fdf9aa641a94392e1a9021a7ab5fd31122ef0
miker704 <miker704@gmail.com>
2024-03-04 07:37:13 -0500

    problem setup

---
+++ java_version/intro/max_value/max_value.java
@@ -0,0 +1,24 @@
+package intro.max_value;
+
+
+public class max_value {
+
+    public static double maxValue(double[] numbers) {
+
+        // List<Double> list =
+        // Arrays.stream(numbers).boxed().collect(Collectors.toList());
+        // Optional<Double> result = list.stream().max((a,b)->a-b);
+        double result = 0.0;
+        
+        return result;
+    }
+
+    public static void main(String[] args) {
+        double[] numbers1 = { 4, 7, 2, 8, 10, 9 };
+        double[] numbers2 = { 10, 5, 40, 40.3 };
+        System.out.println(maxValue(numbers1)); // 10
+        System.out.println(maxValue(numbers2)); // 40.3
+
+    }
+
+}
