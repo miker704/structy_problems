a72c0b58ebfc0ba3c71c048818772fe66ded5dc4
miker704 <miker704@gmail.com>
2024-03-04 03:28:20 -0500

    problem setup

---
+++ javascript_version/intro/hey_programmer/hey_programmer.js
@@ -0,0 +1,8 @@
+//write a functions that takes in a param string s and returns a string 'hey s';
+const greet = (s) => {
+}
+
+
+console.log(greet("alvin")); // -> 'hey alvin'
+console.log(greet("jason")); // -> 'hey jason'
+console.log(greet("how now brown cow")); // -> 'hey how now brown cow'
