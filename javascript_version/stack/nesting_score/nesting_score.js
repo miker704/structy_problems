2450db6b8ba5d56dd7ef2d1dc49f12a0c1a7fb68
miker704 <miker704@gmail.com>
2024-03-04 03:30:17 -0500

    problem setup

---
+++ javascript_version/stack/nesting_score/nesting_score.js
@@ -0,0 +1,29 @@
+
+
+// nesting score
+// Write a function, nestingScore, that takes in a string of brackets as 
+// an argument. The function should return the score of the string according 
+// to the following rules:
+
+// [] is worth 1 point
+// XY is worth m + n points where X, Y are substrings of well-formed brackets and 
+// m, n are their respective scores
+// [S] is worth 2 * k points where S is a substring of well-formed brackets and k
+//  is the score of that substring
+// You may assume that the input only contains well-formed square brackets.
+
+
+const nestingScore = (str) => {
+
+
+};
+
+
+console.log(nestingScore("[]")); // -> 1
+console.log(nestingScore("[][][]")); // -> 3
+console.log(nestingScore("[[]]")); // -> 2
+console.log(nestingScore("[[][]]")); // -> 4
+console.log(nestingScore("[[][][]]")); // -> 6
+console.log(nestingScore("[[][]][]")); // -> 5
+console.log(nestingScore("[][[][]][[]]")); // -> 7
+console.log(nestingScore("[[[[[[[][]]]]]]][]")); // -> 129
