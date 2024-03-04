1ee76a17f0a150bda0361f03dd09aab1974e0926
miker704 <miker704@gmail.com>
2024-03-04 03:29:39 -0500

    problem setup

---
+++ javascript_version/mixed_recall/linked_list_cycle/linked_list_cycle.js
@@ -0,0 +1,115 @@
+// linked list cycle
+// Write a function, linked_list_cycle, that takes
+//  in the head of a linked list as an argument.
+//   The function should return a boolean indicating whether
+//    or not the linked list contains a cycle.
+
+class Node {
+    constructor (val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+// # n = number of nodes
+// # Time: O(n)
+// # Space: O(n)
+const linkedListCycleSet = (head) => {
+    // todo
+
+
+};
+
+
+
+
+// # n = number of nodes
+// # Time: O(n)
+// # Space: O(1)
+
+const linkedListCycleDualPointers = (head) => {
+    // todo
+
+};
+
+const a = new Node('a');
+const b = new Node('b');
+const c = new Node('c');
+const d = new Node('d');
+
+a.next = b;
+b.next = c;
+c.next = d;
+d.next = b; // cycle
+
+//         _______
+//       /        \
+// a -> b -> c -> d 
+
+console.log(linkedListCycleSet(a));  // true
+console.log(linkedListCycleDualPointers(a));  // true
+
+const q = new Node('q');
+const r = new Node('r');
+const s = new Node('s');
+const t = new Node('t');
+const u = new Node('u');
+
+q.next = r;
+r.next = s;
+s.next = t;
+t.next = u;
+u.next = q; // cycle
+
+//    ________________
+//  /                 \
+// q -> r -> s -> t -> u 
+
+console.log(linkedListCycleSet(q));  // true
+console.log(linkedListCycleDualPointers(q));  // true
+
+// const a = new Node('a');
+// const b = new Node('b');
+// const c = new Node('c');
+// const d = new Node('d');
+
+a.next = b;
+b.next = c;
+c.next = d;
+d.next = null;
+// a -> b -> c -> d 
+
+console.log(linkedListCycleSet(a));  // false
+console.log(linkedListCycleDualPointers(a));  // false
+
+// const q = new Node('q');
+// const r = new Node('r');
+// const s = new Node('s');
+// const t = new Node('t');
+// const u = new Node('u');
+
+
+
+
+q.next = r;
+r.next = s;
+s.next = t;
+t.next = u;
+u.next = t; // cycle
+
+//                   __
+//                 /   \
+// q -> r -> s -> t -> u 
+
+console.log(linkedListCycleSet(q));  // true
+console.log(linkedListCycleDualPointers(q));  // true
+
+const p = new Node('p');
+
+// p
+
+console.log(linkedListCycleSet(p)); // false
+console.log(linkedListCycleDualPointers(p)); // false
+
+console.log(linkedListCycleSet(null)); // false
+console.log(linkedListCycleDualPointers(null)); // false
+
