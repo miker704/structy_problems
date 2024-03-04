6e1aa5bebda354af092e101419d51b6d51248871
miker704 <miker704@gmail.com>
2024-03-04 03:09:56 -0500

    problem setup

---
+++ javascript_version/array_and_strings/most_frequent_char/most_frequent_char.js
@@ -0,0 +1,21 @@
+// most frequent char
+// Write a function, mostFrequentChar, that takes in a string as an argument. 
+// The function should return the most frequent character of the string. 
+// If there are ties, return the character that appears earlier in the string.
+
+// You can assume that the input string is non-empty.
+
+const mostFrequentChar = (s) => {
+    
+};
+
+
+
+
+console.log(mostFrequentChar('bookeeper')); // -> 'e'
+console.log(mostFrequentChar('david')); // -> 'd'
+console.log(mostFrequentChar('abby')); // -> 'b'
+console.log(mostFrequentChar('mississippi')); // -> 'i'
+console.log(mostFrequentChar('potato')); // -> 'o'
+console.log(mostFrequentChar('eleventennine')); // -> 'e'
+console.log(mostFrequentChar("riverbed")); // -> 'r'
