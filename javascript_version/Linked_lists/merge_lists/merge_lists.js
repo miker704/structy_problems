566c333f6837927281a0cf6c486a85771b897a41
miker704 <miker704@gmail.com>
2024-03-05 03:20:35 -0500

    finished problem

--- javascript_version/Linked_lists/merge_lists/merge_lists.js
+++ javascript_version/Linked_lists/merge_lists/merge_lists.js
@@ -18,9 +18,52 @@ class Node {
 const mergeLists = (head1, head2) => {
     // todo
 
+    let dead_node = new Node(0);
+    let tail = dead_node;
+    let curr1 = head1;
+    let curr2 = head2;
+
+    while (curr1 !== null && curr2 !== null) {
+
+        if (curr1.val < curr2.val) {
+            tail.next = curr1;
+            curr1 = curr1.next;
+        }
+        else {
+            tail.next = curr2;
+            curr2 = curr2.next;
+        }
+
+        tail = tail.next;
+    }
+
+    if (curr1 !== null) {
+        tail.next = curr1;
+    }
+    if (curr2 !== null) {
+        tail.next = curr2;
+    }
+
+    return dead_node.next;
+
 };
 const mergeListsRecur = (head1, head2) => {
     // todo
+    if (head1 === null) {
+        return head2;
+    }
+    if (head2 === null) {
+        return head1;
+    }
+
+    if (head1.val < head2.val) {
+        head1.next = mergeListsRecur(head1.next, head2);
+        return head1;
+    }
+    else {
+        head2.next = mergeListsRecur(head1, head2.next);
+        return head2;
+    }
 
 
 };
