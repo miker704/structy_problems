cfafc1b11699560f8c5690e195b6961c69d4b3b9
miker704 <miker704@gmail.com>
2024-03-05 03:20:01 -0500

    finished problem

--- javascript_version/array_and_strings/intersection/intersection.js
+++ javascript_version/array_and_strings/intersection/intersection.js
@@ -6,7 +6,12 @@
 // You may assume that each input vector does not contain duplicate elements.
 
 const intersection = (a, b) => {
-
+    let setA = new Set(a);
+    let result = [];
+    for (let item of b) {
+        if (setA.has(item)) { result.push(item); }
+    }
+    return result.sort((a, b) => a - b);
 }
 
 console.log(intersection([4, 2, 1, 6], [3, 6, 9, 2, 10])); // -> [2,6]
