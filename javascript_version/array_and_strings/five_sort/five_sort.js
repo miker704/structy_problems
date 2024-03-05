f8e56db8efbd8639367a9c6261474b3ee68e83ad
miker704 <miker704@gmail.com>
2024-03-05 03:19:58 -0500

    finished problem

--- javascript_version/array_and_strings/five_sort/five_sort.js
+++ javascript_version/array_and_strings/five_sort/five_sort.js
@@ -13,6 +13,24 @@
 
 const fiveSort = (nums) => {
 
+    let i = 0;
+    let j = nums.length - 1;
+    while (i < j) {
+        if (nums[j] === 5) {
+            j--;
+        }
+
+        else if (nums[i] === 5) {
+            [nums[i], nums[j]] = [nums[j], nums[i]];
+            i++;
+        }
+        else {
+            i++;
+        }
+
+
+    }
+    return nums;
 }
 
 
