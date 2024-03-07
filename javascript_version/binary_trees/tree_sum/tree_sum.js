8be36c0f75ef25ba618cf355be030b3854db6eeb
miker704 <miker704@gmail.com>
2024-03-06 22:09:57 -0500

    finished problem

--- javascript_version/binary_trees/tree_sum/tree_sum.js
+++ javascript_version/binary_trees/tree_sum/tree_sum.js
@@ -3,72 +3,89 @@
 // that contains number values. The function should return the total sum of all values in the tree.
 
 class Node {
-    constructor(val) {
-      this.val = val;
-      this.left = null;
-      this.right = null;
+  constructor (val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+const treeSum = (root) => {
+  // todo 
+  if (root === null) { return 0; }
+  let queue = [root];
+  let sum = 0;
+  while (queue.length !== 0) {
+
+    let curr = queue.shift();
+    sum += curr.val;
+
+    if (curr.left !== null) {
+      queue.push(curr.left);
+    }
+    if (curr.right !== null) {
+      queue.push(curr.right);
     }
   }
-  
-  const treeSum = (root) => {
-      // todo
 
-    };
-    
-    const treeSumRecur = (root) => {
+  return sum;
 
-    };
-  
-  const a = new Node(3);
-  const b = new Node(11);
-  const c = new Node(4);
-  const d = new Node(4);
-  const e = new Node(-2);
-  const f = new Node(1);
-  
-  a.left = b;
-  a.right = c;
-  b.left = d;
-  b.right = e;
-  c.right = f;
-  
-  //       3
-  //    /    \
-  //   11     4
-  //  / \      \
-  // 4   -2     1
-  
-  console.log(treeSum(a)); // -> 21
-  console.log(treeSumRecur(a)); // -> 21
-  
-  const a1 = new Node(1);
-  const b1 = new Node(6);
-  const c1 = new Node(0);
-  const d1 = new Node(3);
-  const e1 = new Node(-6);
-  const f1 = new Node(2);
-  const g1 = new Node(2);
-  const h1 = new Node(2);
-  
-  a1.left = b1;
-  a1.right = c1;
-  b1.left = d1;
-  b1.right = e1;
-  c1.right = f1;
-  e1.left = g1;
-  f1.right = h1;
-  
-  //      1
-  //    /   \
-  //   6     0
-  //  / \     \
-  // 3   -6    2
-  //    /       \
-  //   2         2
-  
-  console.log(treeSum(a1)); // -> 10
-  console.log(treeSumRecur(a1)); // -> 10
-  
-  console.log(treeSum(null)); // -> 0
-  console.log(treeSumRecur(null)); // -> 0
-  
+};
+
+const treeSumRecur = (root) => {
+  if (root == null) { return 0; }
+  return root.val + treeSumRecur(root.left) + treeSumRecur(root.right);
+};
+
+const a = new Node(3);
+const b = new Node(11);
+const c = new Node(4);
+const d = new Node(4);
+const e = new Node(-2);
+const f = new Node(1);
+
+a.left = b;
+a.right = c;
+b.left = d;
+b.right = e;
+c.right = f;
+
+//       3
+//    /    \
+//   11     4
+//  / \      \
+// 4   -2     1
+
+console.log(treeSum(a)); // -> 21
+console.log(treeSumRecur(a)); // -> 21
+
+const a1 = new Node(1);
+const b1 = new Node(6);
+const c1 = new Node(0);
+const d1 = new Node(3);
+const e1 = new Node(-6);
+const f1 = new Node(2);
+const g1 = new Node(2);
+const h1 = new Node(2);
+
+a1.left = b1;
+a1.right = c1;
+b1.left = d1;
+b1.right = e1;
+c1.right = f1;
+e1.left = g1;
+f1.right = h1;
+
+//      1
+//    /   \
+//   6     0
+//  / \     \
+// 3   -6    2
+//    /       \
+//   2         2
+
+console.log(treeSum(a1)); // -> 10
+console.log(treeSumRecur(a1)); // -> 10
+
+console.log(treeSum(null)); // -> 0
+console.log(treeSumRecur(null)); // -> 0
