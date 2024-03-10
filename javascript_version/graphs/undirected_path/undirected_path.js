205c554609a7016e2ab8a832c0a2e442cd3e83f2
miker704 <miker704@gmail.com>
2024-03-09 21:22:13 -0500

    finished problem

--- javascript_version/graphs/undirected_path/undirected_path.js
+++ javascript_version/graphs/undirected_path/undirected_path.js
@@ -13,150 +13,195 @@
 
 //this is recursive dfs 
 const undirectedPath = (edges, nodeA, nodeB) => {
+  let graph = buildUndirectedGraph(edges);
+  let visited = new Set();
+  return hasPath(graph, nodeA, nodeB, visited);
+};
 
-  };
-  
-  
-  const undirectedPathDFSIter = (edges, nodeA, nodeB) => {
+
+const undirectedPathDFSIter = (edges, nodeA, nodeB) => {
+  let graph = buildUndirectedGraph(edges);
+  let stack = [nodeA];
+  let visited = new Set();
+  while (stack.length !== 0) {
+    let curr = stack.pop();
+    visited.add(curr);
+    if (curr === nodeB) { return true; }
+    for (let pair of graph[curr]) {
+      if (!visited.has(pair)) {
+        stack.push(pair);
+      }
+    }
+  }
+
+  return false;
+}
+
+const hasPath = (graph, nodeA, nodeB, visited) => {
+  if (nodeA === nodeB) { return true; }
+  if (visited.has(nodeA)) {
+    return false;
+  }
+  visited.add(nodeA);
+  for (let currNode of graph[nodeA]) {
+    if (hasPath(graph, currNode, nodeB, visited)) {
+      return true;
+    }
+  }
+  return false;
+}
 
 
-    
+const undirectedPathBFS = (edges, nodeA, nodeB) => {
+  let graph = buildUndirectedGraph(edges);
+  let visited = new Set();
+  let queue = [nodeA];
+  while (queue.length !== 0) {
+    let curr = queue.shift();
+    visited.add(curr);
+    if (curr === nodeB) { return true; }
+    for (let pair of graph[curr]) {
+      if (!visited.has(pair)) {
+        queue.push(pair);
+      }
+    }
   }
-  
-  const hasPath = (graph, nodeA, nodeB, visited) => {
 
+  return false;
+}
+
+
+const buildUndirectedGraph = (edges) => {
+  let graph = [];
+  for (let path of edges) {
+    let x = path[0];
+    let y = path[1];
+    if (!(x in graph)) { graph[x] = []; }
+    if (!(y in graph)) { graph[y] = []; }
+    graph[x].push(y);
+    graph[y].push(x);
   }
-  
-  
-  const undirectedPathBFS = (edges, nodeA, nodeB) => {
-  
-  }
-  
-  
-  const buildUndirectedGraph = (edges) => {
-  
-    
-  }
-  
-  
-  let edges = [
-    ['i', 'j'],
-    ['k', 'i'],
-    ['m', 'k'],
-    ['k', 'l'],
-    ['o', 'n']
-  ];
-  
-  // console.table(buildUndirectedGraph(edges));
-  console.log(undirectedPath(edges, 'j', 'm')); // -> true
-  // console.log(undirectedPathDFSIter(edges, 'j', 'm')); // -> true
-  // console.log(undirectedPathBFS(edges, 'j', 'm')); // -> true
-  
-  edges = [
-    ['i', 'j'],
-    ['k', 'i'],
-    ['m', 'k'],
-    ['k', 'l'],
-    ['o', 'n']
-  ];
-  console.log(undirectedPath(edges, 'm', 'j'));// -> true
-  // console.log(undirectedPathDFSIter(edges, 'm', 'j'));// -> true
-  // console.log(undirectedPathBFS(edges, 'm', 'j'));// -> true
-  
-  edges = [
-    ['i', 'j'],
-    ['k', 'i'],
-    ['m', 'k'],
-    ['k', 'l'],
-    ['o', 'n']
-  ];
-  console.log(undirectedPath(edges, 'l', 'j'));// -> true
-  // console.log(undirectedPathDFSIter(edges, 'l', 'j'));// -> true
-  // console.log(undirectedPathBFS(edges, 'l', 'j'));// -> true
-  
-  edges = [
-    ['i', 'j'],
-    ['k', 'i'],
-    ['m', 'k'],
-    ['k', 'l'],
-    ['o', 'n']
-  ];
-  console.log(undirectedPath(edges, 'k', 'o'));// -> false
-  // console.log(undirectedPathDFSIter(edges, 'k', 'o'));// -> false
-  // console.log(undirectedPathBFS(edges, 'k', 'o'));// -> false
-  
-  edges = [
-    ['i', 'j'],
-    ['k', 'i'],
-    ['m', 'k'],
-    ['k', 'l'],
-    ['o', 'n']
-  ];
-  console.log(undirectedPath(edges, 'i', 'o'));// -> false
-  // console.log(undirectedPathDFSIter(edges, 'i', 'o'));// -> false
-  // console.log(undirectedPathBFS(edges, 'i', 'o'));// -> false
-  
-  edges = [
-    ['b', 'a'],
-    ['c', 'a'],
-    ['b', 'c'],
-    ['q', 'r'],
-    ['q', 's'],
-    ['q', 'u'],
-    ['q', 't'],
-  ];
-  
-  console.log(undirectedPath(edges, 'a', 'b'));// -> true
-  // console.log(undirectedPathDFSIter(edges, 'a', 'b'));// -> true
-  // console.log(undirectedPathBFS(edges, 'a', 'b'));// -> true
-  
-  edges = [
-    ['b', 'a'],
-    ['c', 'a'],
-    ['b', 'c'],
-    ['q', 'r'],
-    ['q', 's'],
-    ['q', 'u'],
-    ['q', 't'],
-  ];
-  
-  console.log(undirectedPath(edges, 'a', 'c'));  // -> true
-  // console.log(undirectedPathDFSIter(edges, 'a', 'c'));  // -> true
-  // console.log(undirectedPathBFS(edges, 'a', 'c'));  // -> true
-  
-  edges = [
-    ['b', 'a'],
-    ['c', 'a'],
-    ['b', 'c'],
-    ['q', 'r'],
-    ['q', 's'],
-    ['q', 'u'],
-    ['q', 't'],
-  ];
-  console.log(undirectedPath(edges, 'r', 't'));// -> true
-  // console.log(undirectedPathDFSIter(edges, 'r', 't'));// -> true
-  // console.log(undirectedPathBFS(edges, 'r', 't'));// -> true
-  
-  edges = [
-    ['b', 'a'],
-    ['c', 'a'],
-    ['b', 'c'],
-    ['q', 'r'],
-    ['q', 's'],
-    ['q', 'u'],
-    ['q', 't'],
-  ];
-  console.log(undirectedPath(edges, 'r', 'b'));// -> false
-  // console.log(undirectedPathDFSIter(edges, 'r', 'b'));// -> false
-  // console.log(undirectedPathBFS(edges, 'r', 'b'));// -> false
-  
-  edges = [
-    ['s', 'r'],
-    ['t', 'q'],
-    ['q', 'r'],
-  ];
-  console.log(undirectedPath(edges, 'r', 't'));// -> true
-  // console.log(undirectedPathDFSIter(edges, 'r', 't'));// -> true
-  // console.log(undirectedPathBFS(edges, 'r', 't'));// -> true
-  
-  
+  return graph;
+}
+
+
+let edges = [
+  ['i', 'j'],
+  ['k', 'i'],
+  ['m', 'k'],
+  ['k', 'l'],
+  ['o', 'n']
+];
+
+// console.table(buildUndirectedGraph(edges));
+console.log(undirectedPath(edges, 'j', 'm')); // -> true
+console.log(undirectedPathDFSIter(edges, 'j', 'm')); // -> true
+console.log(undirectedPathBFS(edges, 'j', 'm')); // -> true
+
+edges = [
+  ['i', 'j'],
+  ['k', 'i'],
+  ['m', 'k'],
+  ['k', 'l'],
+  ['o', 'n']
+];
+console.log(undirectedPath(edges, 'm', 'j'));// -> true
+console.log(undirectedPathDFSIter(edges, 'm', 'j'));// -> true
+console.log(undirectedPathBFS(edges, 'm', 'j'));// -> true
+
+edges = [
+  ['i', 'j'],
+  ['k', 'i'],
+  ['m', 'k'],
+  ['k', 'l'],
+  ['o', 'n']
+];
+console.log(undirectedPath(edges, 'l', 'j'));// -> true
+console.log(undirectedPathDFSIter(edges, 'l', 'j'));// -> true
+console.log(undirectedPathBFS(edges, 'l', 'j'));// -> true
+
+edges = [
+  ['i', 'j'],
+  ['k', 'i'],
+  ['m', 'k'],
+  ['k', 'l'],
+  ['o', 'n']
+];
+console.log(undirectedPath(edges, 'k', 'o'));// -> false
+console.log(undirectedPathDFSIter(edges, 'k', 'o'));// -> false
+console.log(undirectedPathBFS(edges, 'k', 'o'));// -> false
+
+edges = [
+  ['i', 'j'],
+  ['k', 'i'],
+  ['m', 'k'],
+  ['k', 'l'],
+  ['o', 'n']
+];
+console.log(undirectedPath(edges, 'i', 'o'));// -> false
+console.log(undirectedPathDFSIter(edges, 'i', 'o'));// -> false
+console.log(undirectedPathBFS(edges, 'i', 'o'));// -> false
+
+edges = [
+  ['b', 'a'],
+  ['c', 'a'],
+  ['b', 'c'],
+  ['q', 'r'],
+  ['q', 's'],
+  ['q', 'u'],
+  ['q', 't'],
+];
+
+console.log(undirectedPath(edges, 'a', 'b'));// -> true
+console.log(undirectedPathDFSIter(edges, 'a', 'b'));// -> true
+console.log(undirectedPathBFS(edges, 'a', 'b'));// -> true
+
+edges = [
+  ['b', 'a'],
+  ['c', 'a'],
+  ['b', 'c'],
+  ['q', 'r'],
+  ['q', 's'],
+  ['q', 'u'],
+  ['q', 't'],
+];
+
+console.log(undirectedPath(edges, 'a', 'c'));  // -> true
+console.log(undirectedPathDFSIter(edges, 'a', 'c'));  // -> true
+console.log(undirectedPathBFS(edges, 'a', 'c'));  // -> true
+
+edges = [
+  ['b', 'a'],
+  ['c', 'a'],
+  ['b', 'c'],
+  ['q', 'r'],
+  ['q', 's'],
+  ['q', 'u'],
+  ['q', 't'],
+];
+console.log(undirectedPath(edges, 'r', 't'));// -> true
+console.log(undirectedPathDFSIter(edges, 'r', 't'));// -> true
+console.log(undirectedPathBFS(edges, 'r', 't'));// -> true
+
+edges = [
+  ['b', 'a'],
+  ['c', 'a'],
+  ['b', 'c'],
+  ['q', 'r'],
+  ['q', 's'],
+  ['q', 'u'],
+  ['q', 't'],
+];
+console.log(undirectedPath(edges, 'r', 'b'));// -> false
+console.log(undirectedPathDFSIter(edges, 'r', 'b'));// -> false
+console.log(undirectedPathBFS(edges, 'r', 'b'));// -> false
+
+edges = [
+  ['s', 'r'],
+  ['t', 'q'],
+  ['q', 'r'],
+];
+console.log(undirectedPath(edges, 'r', 't'));// -> true
+console.log(undirectedPathDFSIter(edges, 'r', 't'));// -> true
+console.log(undirectedPathBFS(edges, 'r', 't'));// -> true
+
