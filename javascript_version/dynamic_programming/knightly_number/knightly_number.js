82bd1c451c3e1633366a412c1f4f1584a297e360
miker704 <miker704@gmail.com>
2024-03-14 08:16:31 -0400

    finished problem

--- javascript_version/dynamic_programming/knightly_number/knightly_number.js
+++ javascript_version/dynamic_programming/knightly_number/knightly_number.js
@@ -22,36 +22,38 @@
 
 const knightlyNumber = (n, m, kr, kc, pr, pc) => {
     // todo
+    return _knightlyNumber(n, m, kr, kc, pr, pc, {});
 };
 
 // const _knightlyNumber = (n, m, kr, kc, pr, pc, hash) => {
-//     let pos = `${m},${kr},${kc}`;
-//     if (pos in hash) {
-//         return hash[pos];
-//     }
+//     let key = `${kr},${kc},${m}`;
+//     if (key in hash) { return hash[key]; }
 //     if (m === 0) {
 //         if (kr === pr && kc === pc) {
 //             return 1;
 //         }
-//         else {
+//         else{
 //             return 0;
 //         }
 //     }
-//     let count = 0;
-//     let horseyMoves = knightMovement(n, kr, kc);
-//     for (let move of horseyMoves) {
-//         let [row, col] = move;
-
-//         count += _knightlyNumber(n, m - 1, row, col, pr, pc, hash);
+//     if (!isBoardBound(n, kr, kc)) { return 0; }
+//     let validMoves = knightMovement(n, kr, kc);
+//     let numOfMoves = 0;
+//     for (let move of validMoves) {
+//         let [newKr, newKc] = move;
+//         numOfMoves += _knightlyNumber(n, m - 1, newKr, newKc, pr, pc, hash);
 //     }
-//     hash[pos] = count;
-//     return hash[pos];
-
+//     hash[key] = numOfMoves;
+//     return hash[key];
 // };
 
+// const isBoardBound = (n, kr, kc) => {
+//     let rBound = 0 <= kr && kr < n;
+//     let cBound = 0 <= kc && kc < n;
+//     return rBound && cBound;
+// }
 
 // const knightMovement = (n, kr, kc) => {
-
 //     let horseyMoves = [
 //         [kr + -2, kc + 1],
 //         [kr + -2, kc + -1],
@@ -62,32 +64,59 @@ const knightlyNumber = (n, m, kr, kc, pr, pc) => {
 //         [kr + -1, kc + -2],
 //         [kr + 1, kc + -2]
 //     ]
-
 //     let validMoves = [];
-//     for (let moves of horseyMoves) {
-//         let [row, col] = moves;
+
+//     for (let move of horseyMoves) {
+//         let [row, col] = move;
 //         if (isBoardBound(n, row, col)) {
-//             validMoves.push(moves);
+//             validMoves.push(move);
 //         }
 //     }
-//     // console.log("valid moves");
-//     // console.table(validMoves);
+
 //     return validMoves;
 // }
 
-
-// const isBoardBound = (n, kr, kc) => {
-//     let rBound = 0 <= kr && kr < n;
-//     let cBound = 0 <= kc && kc < n;
-//     return rBound && cBound;
-// }
-
+//we can rewrite the above code to this same performance of o(m^8) even with memoization that 
+// fact we can move 8 different ways per move for each move means 8  branches are create again
+// per move which is unavoidable.
 const _knightlyNumber = (n, m, kr, kc, pr, pc, hash) => {
+    let pos = `${m},${kr},${kc}`;
+    if (pos in hash) {
+        return hash[pos];
+    }
 
+    if (kr < 0 || kr >= n || kc < 0 || kc >= n) {
+        return 0;
+    }
+
+    if (m === 0) {
+        if (kr === pr && kc === pc) {
+            return 1;
+        }
+        else {
+            return 0;
+        }
+    }
+    let count = 0;
+    let horseyMoves = [
+        [kr + -2, kc + 1],
+        [kr + -2, kc + -1],
+        [kr + 2, kc + 1],
+        [kr + 2, kc + -1],
+        [kr + -1, kc + 2],
+        [kr + 1, kc + 2],
+        [kr + -1, kc + -2],
+        [kr + 1, kc + -2]
+    ]
+    for (let move of horseyMoves) {
+        let [row, col] = move;
+
+        count += _knightlyNumber(n, m - 1, row, col, pr, pc, hash);
+    }
+    hash[pos] = count;
+    return hash[pos];
 
 };
-
-
 
 console.log(knightlyNumber(8, 2, 4, 4, 5, 5)); // -> 2
 console.log(knightlyNumber(8, 2, 7, 1, 7, 1)); // -> 3
