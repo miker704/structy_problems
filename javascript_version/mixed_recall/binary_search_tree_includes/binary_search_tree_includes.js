e2e9a33906d8c949724da5b1a932d75fc191ebe6
miker704 <miker704@gmail.com>
2024-03-18 17:37:49 -0400

    finished problem

--- javascript_version/mixed_recall/binary_search_tree_includes/binary_search_tree_includes.js
+++ javascript_version/mixed_recall/binary_search_tree_includes/binary_search_tree_includes.js
@@ -18,7 +18,10 @@ class Node {
 }
 
 const binarySearchTreeIncludes = (root, target) => {
-
+    if (root === null) { return false; }
+    if (root.val === target) { return true; }
+    if (root.val > target) { return binarySearchTreeIncludes(root.left, target); }
+    else { return binarySearchTreeIncludes(root.right, target); }
 };
 
 const a = new Node(12);
