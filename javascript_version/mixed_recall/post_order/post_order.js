f6b78415688285aa95f621fb56dfd9cf03e123a0
miker704 <miker704@gmail.com>
2024-03-18 17:53:03 -0400

    finished problem

--- javascript_version/mixed_recall/post_order/post_order.js
+++ javascript_version/mixed_recall/post_order/post_order.js
@@ -16,8 +16,10 @@ class Node {
 
 const postOrder = (root) => {
     // todo
-
-
+    if (root === null) { return []; }
+    let left = postOrder(root.left);
+    let right = postOrder(root.right);
+    return [...left, ...right, root.val];
 };
 
 
