bfb24b02cddd0cbc03416ba68bbee5c862118912
miker704 <miker704@gmail.com>
2024-03-18 02:52:16 -0400

    finished problem

--- javascript_version/mixed_recall/tolerant_teams/tolerant_teams.js
+++ javascript_version/mixed_recall/tolerant_teams/tolerant_teams.js
@@ -7,17 +7,39 @@
 
 const tolerantTeams = (rivalries) => {
     // todo
-
+    let colorMapping = {};
+    let graph = buildTeams(rivalries);
+    for (let team in graph) {
+        if (!(team in colorMapping) && parseTeams(graph, colorMapping, team, true) === false) {
+            return false;
+        }
+    }
+    return true;
 };
 
 
 const parseTeams = (graph, colorMapping, node, currColor) => {
-
+    if (node in colorMapping) { return colorMapping[node] === currColor; }
+    colorMapping[node] = currColor;
+    for (let currNode of graph[node]) {
+        if (parseTeams(graph, colorMapping, currNode, !colorMapping[node]) === false) {
+            return false;
+        }
+    }
+    return true;
 }
 
 
 const buildTeams = (rivalries) => {
-
+    let graph = {};
+    for (let pair of rivalries) {
+        let [x, y] = pair;
+        if (!(x in graph)) { graph[x] = []; }
+        if (!(y in graph)) { graph[y] = []; }
+        graph[x].push(y);
+        graph[y].push(x);
+    }
+    return graph;
 }
 
 console.log(tolerantTeams([
