77d951ee0165314ccd336b5d25de32f6d299ee6a
miker704 <miker704@gmail.com>
2024-03-23 20:25:08 -0400

    finished problem

--- java_version/intro/is_prime/is_prime.java
+++ java_version/intro/is_prime/is_prime.java
@@ -3,7 +3,14 @@ package intro.is_prime;
 public class is_prime {
 
     public static boolean isPrime(int number) {
-    
+        if (number < 2) {
+            return false;
+        }
+        for (int i = 2; i < number; i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
         return true;
     }
 
