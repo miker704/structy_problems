2229174a94ce0aaf429a24199e4bff7344ab089b
miker704 <miker704@gmail.com>
2024-03-23 20:25:14 -0400

    finished problem

--- java_version/intro/max_value/max_value.java
+++ java_version/intro/max_value/max_value.java
@@ -9,13 +9,15 @@ public class max_value {
         // Arrays.stream(numbers).boxed().collect(Collectors.toList());
         // Optional<Double> result = list.stream().max((a,b)->a-b);
         double result = 0.0;
-        
+        for (double num : numbers) {
+            result = Math.max(result, num);
+        }
         return result;
     }
 
     public static void main(String[] args) {
-        double[] numbers1 = { 4, 7, 2, 8, 10, 9 };
-        double[] numbers2 = { 10, 5, 40, 40.3 };
+        double[] numbers1 = {4, 7, 2, 8, 10, 9};
+        double[] numbers2 = {10, 5, 40, 40.3};
         System.out.println(maxValue(numbers1)); // 10
         System.out.println(maxValue(numbers2)); // 40.3
 
